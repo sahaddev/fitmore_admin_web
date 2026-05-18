9d55e7d1509153e03702e6bd8092b4b5c2682d46
Sahad M P <sahadmp@Sahads-Mac-mini.local>
2026-05-18 20:24:34 +0530

    update product

--- lib/features/products/presentation/blocs/updateProduct/update_product_bloc.dart
+++ lib/features/products/presentation/blocs/updateProduct/update_product_bloc.dart
@@ -4,12 +4,15 @@ import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 import '../../../domain/entities/product.dart';
+import '../../../domain/usecases/product_usecase.dart';
 
 part 'update_product_event.dart';
 part 'update_product_state.dart';
 part 'update_product_bloc.freezed.dart';
 
 class UpdateProductBloc extends Bloc<UpdateProductEvent, UpdateProductState> {
+  final ProductUsecase _productUsecase = ProductUsecase();
+
   UpdateProductBloc() : super(const UpdateProductStateInitial()) {
     on<_UpdateProduct>(_onUpdateProduct);
   }
@@ -17,5 +20,13 @@ class UpdateProductBloc extends Bloc<UpdateProductEvent, UpdateProductState> {
   FutureOr<void> _onUpdateProduct(
     _UpdateProduct event,
     Emitter<UpdateProductState> emit,
-  ) {}
+  ) async {
+    emit(const UpdateProductState.loading());
+    try {
+      await _productUsecase.updateProduct(event.product.id!, event.product);
+      emit(const UpdateProductState.success());
+    } catch (e) {
+      emit(UpdateProductState.failure(e.toString()));
+    }
+  }
 }
